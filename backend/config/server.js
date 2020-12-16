007facd8f0b9e825eacf66e9717318c3bac40f85
alperaydinjs <alperaydinjs@gmail.com>
2020-12-17 02:03:51 +0300

    ilk commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b71c7977d2767a335a07ba001610d4a5'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b1935fae-fba0-468e-9508-8d54cd39eeaa'
+};
